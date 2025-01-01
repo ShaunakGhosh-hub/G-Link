be9709858d2229c69db0cc6831cecda2c7fa7fb4
Shaunak <123075176+ShaunakGhosh-hub@users.noreply.github.com>
2025-01-01 21:56:20 +0530

    Update App.js\

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function SignIn() {
   return (
     <>
       <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the community guidelines or you will be banned for life!</p>
+      <p> Please Do not violate the community guidelines!</p>
     </>
   );
 }
@@ -92,7 +92,7 @@ function ChatRoom() {
       </main>
 
       <form onSubmit={sendMessage}>
-        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="Type Here" />
         <button type="submit" disabled={!formValue}>🕊️</button>
       </form>
     </>
